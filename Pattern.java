6f34aae351b5ec792b5ec3920776180016141ac7
itsmeankeet <adheekariankit007@gmail.com>
2023-12-23 16:21:21 +0545

    Pattern Added

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,14 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 public class Pattern {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            for(int j = 0; j<=i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+   public Pattern() {
+   }
+
+   public static void main(String[] var0) {
+      for(int var1 = 0; var1 <= 5; ++var1) {
+         for(int var2 = 0; var2 <= var1; ++var2) {
+            System.out.println(var1);
+         }
+      }
+
+   }
 }
