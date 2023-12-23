13cdb3e8104c18c7b8da5108cbae6d599d14892b
itsmeankeet <adheekariankit007@gmail.com>
2023-12-23 16:20:30 +0545

    Multiplicatiion Table Added

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,25 +1,27 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.util.Scanner;
+
 public class MultiplicationTable {
-    int a;
-    int result = 0;
-    public MultiplicationTable(int data)
-    {
-        a = data;
-    }
-    public void table()
-    {
-        for(int i = 0; i<=10; i++)
-        {
-            result = i * a;
-            System.out.println(a+ " * "+ i +" = "+ result );
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int num = sc.nextInt();
-        MultiplicationTable ob = new MultiplicationTable(num);
-        ob.table();
-        sc.close();
-    }
+   int a;
+   int result = 0;
+
+   public MultiplicationTable(int var1) {
+      this.a = var1;
+   }
+
+   public void table() {
+      for(int var1 = 0; var1 <= 10; ++var1) {
+         this.result = var1 * this.a;
+         System.out.println(this.a + " * " + var1 + " = " + this.result);
+      }
+
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the number");
+      int var2 = var1.nextInt();
+      MultiplicationTable var3 = new MultiplicationTable(var2);
+      var3.table();
+   }
 }
