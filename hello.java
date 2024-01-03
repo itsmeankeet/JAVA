a62cc11a31ae87de02d767e83f11e26a51e65cfb
itsmeankeet <adheekariankeet007@gmail.com>
2024-01-03 11:46:51 +0545

    hello java Added

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
-class hello
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
+public class hello {
+
+    public void display() {
+        System.out.println("Hello World!");
     }
-}
+
+    public void displayAuthor() {
+        System.out.println("Program Ending...\n\tAuthor: itsmeankeet");
+    }
+
+    public static void main(String[] args) {
+        hello ob = new hello();
+        ob.display();
+        ob.displayAuthor();
+        return;
+    }
+};
